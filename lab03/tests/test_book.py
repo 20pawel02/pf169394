1bb8259374d21227beb1b6366aebde2aec8a24d6
Pawel Wrobel <wroobel.p@gmail.com>
2025-03-14 14:31:39 +0100

    kroki ćwiczeń - etap 3

--- lab03/tests/test_book.py
+++ lab03/tests/test_book.py
@@ -6,3 +6,13 @@ class TestBookInitialization(unittest.TestCase):
         book = Book("Pride and Prejudice", 432)
         self.assertEqual(book.title, "Pride and Prejudice")
         self.assertEqual(book.page_count, 432)
+
+    def test_calculate_reading_time(self):
+        book_pride_and_prejudice = Book("Pride and Prejudice", 432)
+        book_animal_farm = Book("Animal Farm", 112)
+        self.assertAlmostEqual(book_pride_and_prejudice.read_time, 216, places=3)
+        self.assertAlmostEqual(book_animal_farm.read_time, 56, places=3)
+
+
+if __name__ == '__main__':
+    unittest.main()
