0a8cc50b4471723743a009737e8c9f667331ef7e
Pawel Wrobel <wroobel.p@gmail.com>
2025-03-21 15:15:02 +0100

    kroki ćwiczeń - etap 2

--- lab04/zadaie/src/pesel_validator.py
+++ lab04/zadaie/src/pesel_validator.py
@@ -1,2 +1,5 @@
 class PeselValidator:
-    pass
+    def validate_format(pesel: str):
+        if len(pesel) != 11:
+            return False            
+        return pesel.isdigit()
--- lab04/zadaie/tests/test_pesel_validator.py
+++ lab04/zadaie/tests/test_pesel_validator.py
@@ -1,5 +1,10 @@
 import unittest
 from zadaie.src.pesel_validator import PeselValidator
 
-class TestPeselValidator:
-    pass
+class TestPeselValidator(unittest.TestCase):
+    def test_pesel_format(self):
+        self.assertTrue(PeselValidator.validate_format("12345678901"))
+        self.assertFalse(PeselValidator.validate_format("123456789"))
+        self.assertFalse(PeselValidator.validate_format("1234567890123"))
+        self.assertFalse(PeselValidator.validate_format(""))
+        
