1a5d0249b166dd2145a37ed53a00b04a9c37494f
Pawel Wrobel <wroobel.p@gmail.com>
2025-05-09 14:01:23 +0200

    src - pt.2
    Reservation.py: unifying attributes names for easier understanding. Also moved class Reservation from __init__.py to Reservation.py

--- zaliczenie/src/Reservation.py
+++ zaliczenie/src/Reservation.py
@@ -10,23 +10,23 @@ class Management:
     def __init__(self):
         self.reservations = []
 
-    def booking(self, id: int, name: str, date: str):
+    def booking(self, id: int, user: str, date: str, rooms: int):
         # input validation
         if not id or not isinstance(id, int):
             raise ValueError("User ID must be a valid integer.")
-        if not name or not isinstance(name, str):
+        if not user or not isinstance(user, str):
             raise ValueError("User name must be a valid string.")
         if not date or not isinstance(date, str):
             raise ValueError("Date must be a valid string in 'YYYY-MM-DD' format.")
 
         # checking conflicting reservations
         for existing_reservation in self.reservations:
-            if existing_reservation.id == id and existing_reservation.name == name and existing_reservation.date == date:
-                raise ValueError(f"User {id} {name} already booked room(s) on {date}.")
+            if existing_reservation.id == id and existing_reservation.user == user and existing_reservation.date == date:
+                raise ValueError(f"User {id} {user} already booked room(s) on {date}.")
 
         # creating new reservation id
         newID = len(self.reservations) + 1
-        newReservation = Reservation(id, newID, name, , date, )
+        newReservation = Reservation(id, newID, rooms, user, date)
         self.reservations.append(newReservation)
 
     def cancel_booking(self, id):
@@ -41,4 +41,4 @@ class Management:
         for existing_reservation in self.reservations:
             if existing_reservation.id == id:
                 users.append(existing_reservation)
-            return users
+        return users
