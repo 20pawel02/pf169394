e810bc7a84b3fa6885f6d8f64085ab1c09b177a5
Pawel Wrobel <wroobel.p@gmail.com>
2025-05-13 18:30:27 +0200

    test - pt. x

--- zaliczenie/src/Reservation.py
+++ zaliczenie/src/Reservation.py
@@ -1,3 +1,5 @@
+import re
+
 class Reservation:
     def __init__(
             self, id: int, reservation_number: int, beds: int, user: str, date: str
@@ -15,12 +17,18 @@ class ReservationManagement:
 
     def booking(self, id: int, user: str, date: str, beds: int):
         # input validation
-        if not id or not isinstance(id, int):
+        if not id or not isinstance(id, int) or id < 0:
             raise ValueError("User ID must be a valid integer.")
         if not user or not isinstance(user, str):
             raise ValueError("User name must be a valid string.")
+        if not re.match(r'^[a-zA-Z0-9]+$', user):
+            raise ValueError("User name must contain only letters and numbers.")
         if not date or not isinstance(date, str):
             raise ValueError("Date must be a valid string in 'YYYY-MM-DD' format.")
+        if not re.match(r'^\d{4}-\d{2}-\d{2}$', date):
+            raise ValueError("Date must be a valid string in 'YYYY-MM-DD' format.")
+        if not isinstance(beds, int) or beds < 1:
+            raise ValueError("Number of beds must be a valid integer.")
 
         # checking conflicting reservations
         for existing_reservation in self.reservations:
